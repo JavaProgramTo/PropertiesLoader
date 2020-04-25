08a7434e6be3ad8d8c6095eb7ec9030d08fa7ba1
java-w3schools <venkivenki4b6@gmail.com>
2020-04-25 16:46:22 +0530

    How To Load Outside Property Files In Spring Boot

--- src/main/java/com/javaprogramto/properties/PropertiesLoaderdemo/PropertiesLoaderDemoApplication.java
+++ src/main/java/com/javaprogramto/properties/PropertiesLoaderdemo/PropertiesLoaderDemoApplication.java
@@ -1,13 +1,34 @@
 package com.javaprogramto.properties.PropertiesLoaderdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+@PropertySource("classpath:success-codes.properties")
 @SpringBootApplication
 public class PropertiesLoaderDemoApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(PropertiesLoaderDemoApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(PropertiesLoaderDemoApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(PropertiesLoaderDemoApplication.class,
+				args);
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		logger.info("loaded locations : "+environment.getPropertySources());
+		String welcomeNote = environment.getProperty("welcome.text");
+		logger.info("welcome.text value : " + welcomeNote);
+		
+		// loading from profile
+		
+		String errorCode400 = environment.getProperty("error.400");
+		logger.info("errorCode400 : "+errorCode400);
+		
+		String successCode200 = environment.getProperty("success.200");
+		logger.info("successCode200 : "+successCode200);
 	}
 
 }
