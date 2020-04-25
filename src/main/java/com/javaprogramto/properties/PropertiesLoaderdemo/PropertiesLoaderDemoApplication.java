e3726ea47a6ebfc578a7f36ea8b641d1f4cae0db
Venkatesh Nukala <49858284+java-w3schools@users.noreply.github.com>
2020-04-25 15:29:07 +0530

    initial commit

---
+++ src/main/java/com/javaprogramto/properties/PropertiesLoaderdemo/PropertiesLoaderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javaprogramto.properties.PropertiesLoaderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesLoaderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesLoaderDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaprogramto/properties/PropertiesLoaderdemo/PropertiesLoaderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaprogramto.properties.PropertiesLoaderdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesLoaderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
